2bd8a8557733ee792211913c4c5e52dff3fe0c46
Vitaliy <pentagon0555@gmail.com>
2017-08-27 23:16:18 +0300

    test

--- src/task19/test.java
+++ src/task19/test.java
@@ -2,8 +2,7 @@ package task19;
 
 public class test {
     public static void main(String[] args) {
-        String test = "test";
-        System.out.println(test);
+         System.out.println("test");
 
     }
 }
